6bceb58db631937d91cadfef01e29bd8d80ddd7d
ErnestTester <box.tester@outlook.com>
2026-01-04 17:13:30 -0600

    Add new lines

--- DataFaker/src/main/java/org/example/Main.java
+++ DataFaker/src/main/java/org/example/Main.java
@@ -1,6 +1,8 @@
 package org.example;
 
 import net.datafaker.Faker;//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+import net.datafaker.providers.entertainment.SouthPark;
+
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main
 {
@@ -8,16 +10,17 @@ public class Main
     {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.println("Hello and welcome to create data Fake..!!");
+        System.out.println("Hello and welcome to create data Fake.!!");
 
         Faker faker = new Faker();
         for (int i=0; i<=3; i++)
         {
-            System.out.println("👤 First Name: " + faker.name().firstName());
-            System.out.println("👤 Last Name: " + faker.name().lastName());
-            System.out.println("🏙️ City: " + faker.address().city());
-            System.out.println("📧 Email: " + faker.internet().emailAddress());
-            System.out.println("   Phone:"+ faker.phoneNumber().phoneNumber());
+            System.out.println("First Name: " + faker.name().firstName());
+            System.out.println("Last Name: " + faker.name().lastName());
+            System.out.println("Age:"+ faker.date().birthday(18,75, "yyyy-mm-dd"));
+            System.out.println("City: " + faker.address().city());
+            System.out.println("Country:"+faker.address().country());
+            System.out.println("Phone:"+ faker.phoneNumber().phoneNumber());
             System.out.println();
         }
 
